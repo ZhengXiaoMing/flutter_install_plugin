681ee89d1141de33e0c0fdc7d06528076c1c320b
郑晓铭 <zhengxiaoming@dongqiudi.com>
2021-11-04 17:06:16 +0800

    -swift.h

--- ios/Classes/InstallPlugin.m
+++ ios/Classes/InstallPlugin.m
@@ -1,5 +1,9 @@
 #import "InstallPlugin.h"
+#if __has_include(<install_plugin/install_plugin-Swift.h>)
 #import <install_plugin/install_plugin-Swift.h>
+#else
+#import "install_plugin-Swift.h"
+#endif
 
 @implementation InstallPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
